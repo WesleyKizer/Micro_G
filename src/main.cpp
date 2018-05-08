0c07b516fc6bbadd9ce58645c5415f3d4f28dfff
WesleyKizer <36313577+WesleyKizer@users.noreply.github.com>
2018-05-08 13:06:53 -0500

    Update main.cpp
    new main

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <Wire.h>
 
 test_tube tube1;
+test_tube tube2;
+test_tube tube3;
 
 const int servoPin = 3;
 const int servoMid = 90;
@@ -15,57 +17,35 @@ const int tempPin3 = 8;
 const int servoSpeed = 90;
 const int buttonPin = 2;
 const int eStopPin = 1;
+const int Hall1 = 4;
 
 void setup(){
   Serial.begin(9600);
-  Serial.println("hello world");
+  Serial.println("Setting Up.");
+  Serial.println("");
   tube1.setServo(servoMid, servoPin);
+  tube1.setSw(Hall1);
   pinMode(buttonPin, INPUT);
+
   //tube1.setHeater(heaterPin, setTemp, tempRange);
   //tube1.setTempPins(tempPin1, tempPin2, tempPin3);
   tube1.setUp();
+
+  Serial.println("Set Up Complete.");
+  Serial.println("");
 
 }
 
 void loop(){
 
+  if(Serial.available() > 0){
+    //switch()
+  }
 
-  
-  //tube1.stopServo();
-  //delay(500);
-  //tube1.actForward(servoSpeed);
-  //delay(10000);
-  //tube1.stopServo();
-  //delay(500);
-  //tube1.retract(servoSpeed);
-  //delay(10000);
 
-/*
-  while(!digitalRead(buttonPin)){
-    Serial.println("1");
-    tube1.stopServo();
-  }
-  delay(750);
-  Serial.println("2");
-  while(!digitalRead(buttonPin)){
-    tube1.actForward(servoSpeed);
-    Serial.println("3");
-  }
-  delay(750);
-  Serial.println("4");
-  while(!digitalRead(buttonPin)){
-    tube1.stopServo();
-    Serial.println("5");
-  }
-  delay(750);
-  Serial.println("6");
-  while(!digitalRead(buttonPin)){
-    tube1.retract(servoSpeed);
-    Serial.println("7");
-  }
-  Serial.println("8");
-  delay(750);
-  Serial.println("9");
+  tube1.test(servoSpeed);
 
-  //tube1.stopServo();*/
+
+
 }
+
