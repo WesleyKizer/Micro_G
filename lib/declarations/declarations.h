128491b95db710f1a86dcde08cab3aa32989bfda
WesleyKizer <36313577+WesleyKizer@users.noreply.github.com>
2018-05-28 13:03:51 -0500

    declerations

---
+++ lib/declarations/declarations.h
@@ -0,0 +1,29 @@
+#ifndef _declarations_
+#define _declarations_
+
+
+
+const int mainServoPin = 14;
+const int mainServoMid = 90;
+const int servoPin1 = 5;
+const int servoPin2 = 5;
+const int servoPin3 = 5;
+const int servoMid1 = 92;
+const int servoMid2 = 92;
+const int servoMid3 = 92;
+const int heaterPin1 = 0;
+const int heaterPin2 = 0;
+const int heaterPin3 = 0;
+const int setTemp = 60;
+const int testTemp = 40;
+const int tempRange = 5;
+const int tempPin1 = 11;
+const int tempPin2 = 7;
+const int tempPin3 = 8;
+const int servoSpeed = 50;
+const int Hall1 = 19;
+const int Hall2 = 19;
+const int Hall3 = 19;
+const int mainHall = 19;
+
+#endif
