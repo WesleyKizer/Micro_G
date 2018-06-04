b995bfc3b8cd60ad22a4e72093f3596efb2c12ac
WesleyKizer <36313577+WesleyKizer@users.noreply.github.com>
2018-06-03 19:09:23 -0500

    Update declarations.h

--- lib/declarations/declarations.h
+++ lib/declarations/declarations.h
@@ -3,23 +3,24 @@
 
 
 
-const int mainServoPin = 14;
+const int mainServoPin = 5;
 const int mainServoMid = 90;
-const int servoPin1 = 5;
-const int servoPin2 = 5;
-const int servoPin3 = 5;
+const int servoPin1 = 15;
+const int servoPin2 = 9;
+const int servoPin3 = 10;
 const int servoMid1 = 92;
 const int servoMid2 = 92;
 const int servoMid3 = 92;
-const int heaterPin1 = 0;
-const int heaterPin2 = 0;
-const int heaterPin3 = 0;
-const int tempPin1 = 11;
-const int tempPin2 = 7;
-const int tempPin3 = 8;
-const int Hall1 = 19;
-const int Hall2 = 19;
-const int Hall3 = 19;
-const int mainHall = 19;
+const int heaterPin1 = 21;
+const int heaterPin2 = 20;
+const int heaterPin3 = 19;
+const int tempPin1 = 22;
+const int tempPin2 = 11;
+const int tempPin3 = 12;
+const int Hall1 = 18;
+const int Hall2 = 17;
+const int Hall3 = 16;
+const int mainSW1 = 23;
+const int mainSW2 = 7;
 
 #endif
