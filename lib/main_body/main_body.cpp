cba4802d09b27f800f604a22d351fa70837427aa
WesleyKizer <36313577+WesleyKizer@users.noreply.github.com>
2018-06-03 19:10:20 -0500

    Update main_body.cpp

--- lib/main_body/main_body.cpp
+++ lib/main_body/main_body.cpp
@@ -1,44 +1,50 @@
 #include "main_body.h"
 
 
-void main_body::setAct(int mid, int pin, int sw){
+void main_body::setAct(int mid, int pin, int sw1, int sw2){
   servo_mid = mid;
   servo_pin = pin;
-  sw_pin = sw;
+  sw1_pin = sw1;
+  sw2_pin = sw2;
 }
 
 void main_body::setUp(){
   constServo.attach(servo_pin);
-  pinMode(sw_pin, INPUT);
+  pinMode(sw1_pin, OUTPUT);
+  digitalWrite(sw1_pin, LOW);
+  pinMode(sw2_pin, INPUT_PULLUP);
 
 }
 
 
 void main_body::stopServo(){
-  constServo.write(servo_mid);
+	constServo.detach();
 }
 
 
 void main_body::testAct(int speed){
-	constServo.write(servo_mid - speed);
-	while(digitalRead(sw_pin) == CHANGE){}
+	constServo.attach(servo_pin);
+	while(digitalRead(sw2_pin))constServo.write(servo_mid - speed);
+	constServo.detach();
+	delay(1000);
+	constServo.attach(servo_pin);
 	constServo.write(servo_mid + speed);
-	while(digitalRead(sw_pin) == CHANGE){}
-	constServo.write(servo_mid);
+	delay(10000);
+	constServo.detach();
 
 }
 
 void main_body::actForward(int speed){
- 
-  constServo.write(servo_mid - speed);
-  while(digitalRead(sw_pin) == CHANGE){}
-  constServo.write(servo_mid);
+	constServo.attach(servo_pin);
+	while(digitalRead(sw2_pin))constServo.write(servo_mid - speed);
+	constServo.detach();
 
 }
 
 void main_body::actBackward(int speed){
-	constServo.write(servo_mid + speed);
-	while(digitalRead(sw_pin) == CHANGE){}
-	constServo.write(servo_mid);
+	//constServo.attach(servo_pin);
+	//while(digitalRead(sw_pin) == CHANGE)constServo.write(servo_mid + speed);
+	//constServo.detach();
 
 }
+
